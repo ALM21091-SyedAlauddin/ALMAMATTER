7bad37016abf125bf2eca225a682f9342ace4412
AlauddinSyed <87085169+AlauddinSyed@users.noreply.github.com>
2021-07-11 19:47:53 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,14 @@
+package almamatter;
+
+public class Casting {
+	public static void main(String[] args) {
+		byte b=20;
+		byte c=2;
+
+		int myint=b*c;
+		int myintvalue=257;
+		byte mybitevalue=(byte)myintvalue;
+		System.out.println(mybitevalue);
+		}
+
+}
